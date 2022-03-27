0f616d49e4aaa06ff1546596892444b0ffe9a5ae
Akshata72 <sableakshata880@gmail.com>
2022-03-27 23:09:43 +0530

    Added:UC1-Find_Max_Int_Number

---
+++ Test_Maximun/MaxIntNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Maximun
+{
+    internal class MaxIntNumber
+    {
+        public int NumberMax(int Num1, int Num2, int Num3)
+        {
+            Console.WriteLine("Three numbers are Num1 = {0} , Num2 = {1} , Num3 = {2} ", Num1, Num2, Num3);
+
+            if (Num1.CompareTo(Num2) > 0 && Num1.CompareTo(Num3) > 0)
+            {
+                Console.WriteLine("Maximum = ");
+                return Num1;
+            }
+            else if (Num2.CompareTo(Num3) > 0 && Num2.CompareTo(Num1) > 0)
+            {
+                Console.WriteLine("Maximum = ");
+                return Num2;
+            }
+            else if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0)
+            {
+                Console.WriteLine("Maximum = ");
+                return Num3;
+            }
+            else
+            {
+                Console.WriteLine("all numbers are equal");
+            }
+            return 0;
+        }
+    }
+}
---
+++ Test_Maximun/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Three Numbers to Cheak Maximum Number:");
+            MaxIntNumber maxintnum = new MaxIntNumber();
+            Console.WriteLine("Enter a 1st Number : ");
+            int Num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a 2nd Number : ");
+            int Num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a 3rd Number : ");
+            int Num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(maxintnum.NumberMax(Num1, Num2, Num3));
+        }
+    }
+}
